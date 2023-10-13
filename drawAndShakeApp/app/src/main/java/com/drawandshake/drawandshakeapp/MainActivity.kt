ae2443ffe95743ca4cd8ab00f017aefe3bbbba3e
Squidgy1332 <114405256+w0469100@users.noreply.github.com>
2023-10-13 16:58:42 -0300

    SwitchBrainHurt

--- drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/MainActivity.kt
+++ drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.drawandshake.drawandshakeapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Switch
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        findViewById<Switch>(R.id.menu_switch).setOnCheckedChangeListener { _, isCkecked ->
+            val message = if (isCkecked) "Switch1:ON" else "Switch1:OFF"
+            println(message)
+        }
     }
 }
