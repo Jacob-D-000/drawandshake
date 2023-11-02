7131f4efd44d23df0aaeaf98bc755b3b93b2c3c3
Liam Morton OnlineUser(Squidgy) <114405256+w0469100@users.noreply.github.com>
2023-11-02 10:32:12 -0300

    Fixed buttons happy time

--- drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/ClassicActivity.kt
+++ drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/ClassicActivity.kt
@@ -37,18 +37,18 @@ class ClassicActivity : AppCompatActivity() {
         super.onStart()
         DrawBackArrowEvent(this).backPressed()
 
-        findViewById<ImageButton>(R.id.leftButton).setOnTouchListener { _, motionEvent ->
+        findViewById<ImageButton>(R.id.leftButtonDerection).setOnTouchListener { _, motionEvent ->
             //Will happen when image is pressed other wise event is set to false
             when (motionEvent.action){
                 //change opacity of nob direction image when user holds finger on image
                 MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
-                    findViewById<ImageView>(R.id.leftButtonDerection).alpha = 0.0f
+                    findViewById<ImageButton>(R.id.leftButtonDerection).alpha = 0.0f
 
                     val x: Float = motionEvent.x
                     val y: Float = motionEvent.y
 
-                    val nobCenterX = (findViewById<ImageButton>(R.id.leftButton).x + findViewById<ImageButton>(R.id.leftButton).width) / 2
-                    val nobCenterY = (findViewById<ImageButton>(R.id.leftButton).y + findViewById<ImageButton>(R.id.leftButton).height) / 2
+                    val nobCenterX = findViewById<ImageButton>(R.id.leftButton).width / 2
+                    val nobCenterY = findViewById<ImageButton>(R.id.leftButton).height / 2
                     val angle = Math.toDegrees(atan2(y - nobCenterY, x - nobCenterX).toDouble())
 
                     findViewById<ImageButton>(R.id.leftButton).rotation = angle.toFloat()
@@ -57,7 +57,7 @@ class ClassicActivity : AppCompatActivity() {
                 //when user lets go or the hold action is stopped
                 // it will reset the opacity of the direction image
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    findViewById<ImageView>(R.id.leftButtonDerection).alpha = 1.0f
+                    findViewById<ImageButton>(R.id.leftButtonDerection).alpha = 1.0f
                     findViewById<ImageButton>(R.id.leftButton).rotation = 0f
                 }
             }
