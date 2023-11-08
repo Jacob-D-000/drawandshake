41887f65b46cbd702582af03a19082ee6eaf3def
W0420453 <W0420453@campus.nscc.ca>
2023-11-08 15:53:34 -0400

    Device Sized Canvas, and drawing basic shapes functional

--- drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/TraceActivity.kt
+++ drawAndShakeApp/app/src/main/java/com/drawandshake/drawandshakeapp/TraceActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.graphics.Paint
 
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -18,31 +19,37 @@ import androidx.appcompat.app.AppCompatActivity
  * I also call the Fucktion DrawBackArrow().backPressed() here as well - i will go over that in the file its self
  */
 class TraceActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trace)
 
         val drawingCanvasId = findViewById<ImageView>(R.id.drawingCanvas)
 
-        // These are what I want to include in the createBitmap below
-        // The problem is they always return 0, not the filled screen size
-        println(drawingCanvasId.height)
-        println(drawingCanvasId.width)
+        val displayMetrics = resources.displayMetrics
+        val screenWidth = displayMetrics.widthPixels
+        val screenHeight = displayMetrics.heightPixels
+        println("Width: $screenWidth")
+        println("Height: $screenHeight")
 
 
-        val bitmap: Bitmap = Bitmap.createBitmap(500, 500,Bitmap.Config.ARGB_8888)
+        val bitmap: Bitmap = Bitmap.createBitmap(screenWidth, screenHeight,Bitmap.Config.ARGB_8888)
         val traceCanvas = Canvas(bitmap)
 
         // Example: Draw a red circle on the Canvas
         val paint = Paint()
         paint.color = Color.RED
         traceCanvas.drawCircle(250f, 250f, 100f, paint)
-        
+        traceCanvas.drawCircle(1000f, 1000f, 10f, paint)
+
         drawingCanvasId.setImageBitmap(bitmap)
     }
 
-    override fun onStart(){
+    override fun onStart()
+    {
         super.onStart()
         DrawBackArrowEvent(this).backPressed()
+        println("Height: " + findViewById<ImageView>(R.id.drawingCanvas).height)
+        println("Width: " + findViewById<ImageView>(R.id.drawingCanvas).width)
     }
 }
